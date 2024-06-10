66a189c30d05028c93cc0910f3e724075427d4da
Jeff-Maina <devyoungblud@gmail.com>
2024-06-10 10:56:04 +0300

    Fixed carousel and added shadcn

--- src/app/components/Features/Features.jsx
+++ src/app/components/Features/Features.jsx
@@ -7,58 +7,120 @@ import {
   ArrowRight,
 } from "lucide-react";
 import { features } from "../Data";
+// import FeatureCard from "./FeatureCard";
+// import { AnimatePresence, motion } from "framer-motion";
+// import { useState } from "react";
+
+// const Features = () => {
+//   const [activeIndex, setActiveIndex] = useState(0);
+
+//   const moveForward = () => {
+//     const newIndex = (activeIndex + 1) % features.length;
+//     setActiveIndex(newIndex);
+//   };
+//   const moveBackward = () =>
+//     setActiveIndex(
+//       (prevIndex) => (prevIndex - 1 + features.length) % features.length
+//     );
+//   return (
+//     <>
+//       <main className="w-full flex flex-col gap-10 max-w-7xl m-auto">
+//         <div className="w-full h-[80vh]">
+//           {features.map(
+//             (feature, i) =>
+//               i === activeIndex && (
+//                 <motion.div className="w-full h-full" key={i}>
+//                   <FeatureCard activeIndex={activeIndex} feature={feature} />
+//                 </motion.div>
+//               )
+//           )}
+//         </div>
+
+//         <div className="flex gap-2 items-center justify-end px-8">
+//           <div
+//             onClick={(e) => {
+//               e.preventDefault();
+//               moveBackward();
+//             }}
+//             className="size-16 md:size-20 rounded-full border border-green-600 text-black grid place-items-center"
+//           >
+//             <ArrowLeft />
+//           </div>
+//           <div
+//             onClick={(e) => {
+//               e.preventDefault();
+//               moveForward();
+//             }}
+//             className="size-16 md:size-20 rounded-full border border-green-600 text-black grid place-items-center"
+//           >
+//             <ArrowRight />
+//           </div>
+//         </div>
+//       </main>
+//     </>
+//   );
+// };
+
+// export default Features;
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import FeatureCard from "./FeatureCard";
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const Features = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+const FeaturesCarousel = () => {
+  const [api, setApi] = useState();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const moveForward = () =>
-    setActiveIndex((prevIndex) => (prevIndex + 1) % features.length);
-  const moveBackward = () =>
-    setActiveIndex(
-      (prevIndex) => (prevIndex - 1 + features.length) % features.length
-    );
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
   return (
-    <>
-      <main className="w-full flex flex-col gap-10 max-w-7xl m-auto">
-        <div className="w-full h-[80vh]">
-          <AnimatePresence>
-            {features.map(
-              (feature, i) =>
-                i === activeIndex && (
-                  <motion.div className="w-full h-full" key={i}>
-                    <FeatureCard activeIndex={activeIndex} feature={feature} />
-                  </motion.div>
-                )
-            )}
-          </AnimatePresence>
+    <main className="w-full flex flex-col gap-10 max-w-7xl m-auto lg:h-screen justify-center">
+      <Carousel setApi={setApi}>
+        <CarouselContent>
+          {features.map((feature, i) => {
+            return (
+              <CarouselItem>
+                <FeatureCard feature={feature} key={i} />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <div>
+          <CarouselPrevious className="size-20 border border-green-500 hover:bg-green-200" />
+          <CarouselNext className="size-20 border border-green-500 hover:bg-green-200" />
         </div>
-
-        <div className="flex gap-2 items-center justify-end px-8">
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              moveBackward();
-            }}
-            className="size-16 md:size-20 rounded-full border border-green-600 text-black grid place-items-center"
-          >
-            <ArrowLeft />
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              moveForward();
-            }}
-            className="size-16 md:size-20 rounded-full border border-green-600 text-black grid place-items-center"
-          >
-            <ArrowRight />
-          </button>
+        <div className="flex items-center gap-1 w-full justify-center mt-6">
+          {features.map((feature, i) => {
+            return (
+              <div
+                className={`${
+                  i + 1 === current ? "w-10" : "w-2"
+                } h-2 rounded-full bg-green-500 transition-all duration-200`}
+              ></div>
+            );
+          })}
         </div>
-      </main>
-    </>
+      </Carousel>
+    </main>
   );
 };
 
-export default Features;
+export default FeaturesCarousel;
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="h-16 w-full lg:fixed z-[999] top-0 max-w-[100rem] lg:left-2/4 lg:-translate-x-2/4 p-8 px-8 lg:px-0">
+    <nav className="h-16 w-full z-[999] top-0 max-w-[100rem] lg:left-2/4 lg:-translate-x-2/4 p-8 px-8 lg:px-0">
       <a href="/" className="font-semibold tracking-tighter lg:text-3xl">Adeego</a>
     </nav>
   );
